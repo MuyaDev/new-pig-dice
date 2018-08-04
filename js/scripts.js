ebb59af5495563d2f9ed99f866d2040c5621b34a
¨John <folnett@yahoo.co.uk>
2018-08-04 12:21:17 +0300

    add reset game functionality,add user-logic for button click function

--- js/scripts.js
+++ js/scripts.js
@@ -60,3 +60,56 @@ function switchPlayer() {
 
   }
 }
+
+function resetGame() {
+  pigDice.player1Score = 0;
+  pigDice.player2Score = 0;
+  pigDice.playerUp = 1;
+  pigDice.turnScore = 0;
+}
+
+// USER LOGIC //
+
+
+function alertEndTurn() {
+  alert("Sorry - you rolled a 1.  Your score remains the same and your turn is over.");
+  $(".playerStatus").text(pigDice.playerUp);
+}
+
+function alertWinner(playerNumber) {
+  alert("Player " + playerNumber + " is the  winner!!");
+  resetGame();
+  $(".gameStatusDisplay").text(0);
+}
+
+$(document).ready(function () {
+
+  $("form#pigForm").submit(function (event) {
+    var playerName1 = $("input#Name1").val();
+    var playerName2 = $("input#Name2").val();
+    $("span#playerName1").text(playerName1);
+    $("span#playerName2").text(playerName2);
+    $("#player2Button").hide();
+    $("#player1Button").show();
+    $(".playerStatus").text(pigDice.playerUp);
+    event.preventDefault();
+
+    var nameHolder = new Names(playerName1, playerName2);
+  })
+
+
+  $(".rollPig").click(function () {
+    pigResult = playerRoll();
+    $(".rollResult").text(pigResult);
+    $(".turnScore").text(pigDice.turnScore);
+
+  });
+
+  $(".holdPig").click(function () {
+    holdThePig();
+    $("rollResult").text("");
+    $(".player1Score").text(pigDice.player1Score);
+    $(".player2Score").text(pigDice.player2Score);
+    $(".playerStatus").text(pigDice.playerUp);
+  });
+});
